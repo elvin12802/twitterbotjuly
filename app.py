3b06493aabc19bc00c547af69e20784ec10c819c
elvin12802 <86964173+elvin12802@users.noreply.github.com>
2021-07-05 18:20:11 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import tweepy
 import time
+import os 
 
-consumer_key = '0llZZyMKfpYQYNFrVdPgYib3v'
-consumer_secret = 'yImgAuGQwBnHeRAuIfOXzageySSPT6tvlOEmbKlTi9QoqNUsdI'
-access_token = '1400467529663803396-4hYjuLjxBbwlXxprNjY24PsMv3kur9'
-access_token_secret = '7bZKj3YHa60TMsa35rFwHHO9OVdGdnP7IkggHjM213j8s'
+consumer_key = os.getenv('consumer_key')
+consumer_secret = os.getenv('consumer_secret')
+access_token = os.getenv('access_token')
+access_token_secret = os.getenv('access_token_secret')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
